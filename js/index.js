57c34cfae54d2f5569ed8bbad6e15d047d77c629
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-03-04 14:36:50 -0800

    mvp and stretch

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let codesnippet = document.getElementById("cta-img");
+codesnippet.setAttribute('src', siteContent["cta"]["img-src"])
+
+let midsnippet = document.getElementById("middle-img");
+midsnippet.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// Nav
+const anchors = document.querySelectorAll('a');
+anchors[0].textContent = "Services";
+anchors[1].textContent = "Product";
+anchors[2].textContent = "Vision";
+anchors[3].textContent = "Features";
+anchors[4].textContent = "About";
+anchors[5].textContent = "Contact";
+
+// CTA
+let slogan = document.querySelector('h1');
+slogan.textContent = "DOM IS AWESOMME";
+
+let button = document.querySelector('button');
+button.textContent = "Get Started";
+
+
+// Content
+let headers = document.querySelectorAll('h4');
+headers[0].textContent = "Features";
+headers[1].textContent = "About";
+headers[2].textContent = "Services";
+headers[3].textContent = "Products";
+headers[4].textContent = "Vision";
+headers[5].textContent = "Contact";
+
+let content = document.querySelectorAll('p');
+content[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+content[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+content[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+content[3].textContent = "Prouct content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+content[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+content[5].textContent = "123 Way 456 Street Somewhere, USA";
+content[6].textContent = "1 (888) 888-8888";
+content[7].textContent = "sales@greatidea.io";
+content[8].textContent = "Copyright Great Idea! 2018";
+
+// Stretch
+anchors.forEach(i => i.style.color = "blue");
+button.style.color = "blue";
+headers.forEach(function(i) {
+  i.style.textDecoration = "underline";
+});
+content.forEach(function(i) {
+  i.style.color= "blue";
+});
